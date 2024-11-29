4a18a0f2d5470ddc5bf76a14e76dd2b92282b314
OmarEhab14 <www.omarehab3710@gmail.com>
2024-11-29 14:20:23 +0200

    task finished

--- src/docker_test/Docker_test.java
+++ src/docker_test/Docker_test.java
@@ -2,5 +2,6 @@ package docker_test;
 public class Docker_test {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Omar and this is a docker testing project");
+        System.out.println("This is the project after adding github actions!!");
     }
 }
